d04b8f213e1e45729fcaaf1e618d1c5614865e30
tangqiuqiu <dawn.tang@temu.com>
2025-02-12 22:32:41 +0800

    feat: load gstmore

--- excel2json/index.js
+++ excel2json/index.js
@@ -180,6 +180,119 @@ const getActivityList = () => {
   console.log('songMap 已成功转换为 JSON 文件:', jsonFilePathSong);
 };
 
+const getShowList = () => {
+  // 改为Excel文件路径
+  const jsonFilePath = './public/output/list_show.json';
+  const jsonFilePathSong = './public/output/song_show.json';
+
+  const sheetName = workbook.SheetNames[1];
+  const worksheet = workbook.Sheets[sheetName];
+
+  // 将Excel数据转换为JSON
+  const rows = xlsx.utils.sheet_to_json(worksheet, { header: 1 });
+
+  // 获取表头
+  const headers = rows[0];
+  const headerMap = {
+    演唱歌曲: 'songs',
+    播出时间: 'date',
+    播出平台: 'location',
+    节目名称: 'activity',
+    期数: 'period',
+  };
+
+  const songMap = {};
+  const results = [];
+
+  // 遍历每一行数据
+  for (let rowIndex = 1; rowIndex < rows.length; rowIndex++) {
+    const row = rows[rowIndex];
+    if (!row || row.length === 0) continue;
+
+    const rowData = {};
+
+    const insertSong = (song) => {
+      if (!song) return;
+
+      const songList = song
+        .split('+')
+        ?.map((song) => song.trim())
+        .filter((it) => !!it && it !== '-' && it !== '—');
+      rowData[headerMap['演唱歌曲']].push(...songList);
+      rowData[headerMap['演唱歌曲']] = [
+        ...new Set(rowData[headerMap['演唱歌曲']]),
+      ];
+
+      songList.forEach((songItem) => {
+        if (!songMap[songItem]) {
+          songMap[songItem] = [];
+        }
+        songMap[songItem].push(rowIndex);
+        songMap[songItem] = [...new Set(songMap[songItem])];
+      });
+    };
+
+    const insertLink = (hyperlink) => {
+      rowData.links = hyperlink
+        ? [...(rowData.links || []), hyperlink]
+        : rowData.links;
+    };
+
+    headers.forEach((_header, index) => {
+      const header = String(_header).trim();
+      const cell = worksheet[xlsx.utils.encode_cell({ r: rowIndex, c: index })];
+      const value = cell ? String(cell.v || '') : '';
+
+      if (header.startsWith('演唱歌曲')) {
+        if (!rowData[headerMap['演唱歌曲']]) {
+          rowData[headerMap['演唱歌曲']] = [];
+        }
+        insertSong(value.trim());
+      } else if (!Number.isNaN(Number(header))) {
+        insertSong(value.trim());
+      } else {
+        const headerKey = headerMap[header];
+
+        if (!rowData[headerKey]) {
+          rowData[headerKey] = {};
+        }
+
+        if (headerKey === headerMap['播出时间']) {
+          rowData[headerKey] = getDateStr(value.trim());
+          rowData['timestamp'] = toTimestamp(rowData[headerKey]);
+        } else {
+          rowData[headerKey] = getPureVal(value);
+        }
+      }
+    });
+
+    rowData.type = '综艺晚会';
+    rowData.activity += rowData.period;
+    delete rowData.period;
+    rowData.key = `${rowData.type}_${rowData.date}_${rowData.location}_${rowData.activity}`;
+    results.push(rowData);
+  }
+
+  // 处理歌曲映射
+  const songList = [];
+  Object.keys(songMap).forEach((song) => {
+    songList.push({
+      song,
+      activity: songMap[song].map((index) => results[index - 1].key || ''),
+    });
+  });
+
+  // 写入JSON文件
+  if (!fs.existsSync(path.dirname(jsonFilePath))) {
+    fs.mkdirSync(path.dirname(jsonFilePath));
+  }
+
+  fs.writeFileSync(jsonFilePath, JSON.stringify(results, null, 2));
+  console.log('Excel 文件已成功转换为 JSON 文件:', jsonFilePath);
+  fs.writeFileSync(jsonFilePathSong, JSON.stringify(songList, null, 2));
+  console.log('songMap 已成功转换为 JSON 文件:', jsonFilePathSong);
+};
+
 const getWebLiveList = () => {
   // 改为Excel文件路径
   const jsonFilePath = './public/output/list_web_live.json';
@@ -614,8 +727,9 @@ const getBodCastList = () => {
 };
 
 // getActivityList();
+getShowList();
 // getSongInfo();
 // getWebLiveList();
 // geVideoInterviewList();
 // geTextInterviewList();
-getBodCastList();
+// getBodCastList();
